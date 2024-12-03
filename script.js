c42a572eb70db73d6ef196d1084a928631516748
Audrey Norvez <audrey.norvez@gmail.com>
2024-12-03 17:00:11 +0100

    randomize point

--- script.js
+++ script.js
@@ -3,16 +3,32 @@ let medium = ["-6", "6"];
 let large = ["-10", "10"];
 
 function randomizePoint(array){
-    const max = array[0];
-    const min = array[1];
-    function getRandomInt(max, min) {
-        return Math.floor(Math.random() * (max - min) + min);
-    }
+    const max = parseInt(array[0]);
+    const min = parseInt(array[1]);
+    console.log(Math.floor(Math.random() * (max - min) + min))   
 }
 
 function reveal(){
     const flipBoxes = document.querySelectorAll(".flip_box");
     flipBoxes.forEach(flipBox => {
         flipBox.classList.toggle("flipped");
+        let size = flipBox.getAttribute("bloc-size");
+        let pointArray = [];
+        let point = null;
+
+        switch (size) {
+            case 'small':
+                point = randomizePoint(small);
+                break;
+            case 'medium':
+                point = randomizePoint(medium);
+                break;
+            case 'large':
+                point = randomizePoint(large);
+                break;
+            default:
+                break;
+        }
+        pointArray.push(point);
     });
 }
