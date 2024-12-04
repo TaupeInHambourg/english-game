be91983195d4c4301fa9d75b404ecbd71cd87382
Audrey Norvez <audrey.norvez@gmail.com>
2024-12-04 14:29:33 +0100

    working app

--- script.js
+++ script.js
@@ -1,7 +1,87 @@
 let small = ["-2", "2"];
 let medium = ["-6", "6"];
+let mediumplus = ["-8", "8"];
 let large = ["-10", "10"];
 let pointArray = [];
+
+function addCards(){
+    let cards = [
+        {
+            "size" : "small",
+            "url" : "small-1.png",
+        },
+        {
+            "size" : "small",
+            "url" : "small-2.png",
+        },
+        {
+            "size" : "small",
+            "url" : "small-3.png",
+        },
+        {
+            "size" : "medium",
+            "url" : "medium-1.png",
+        },
+        {
+            "size" : "medium",
+            "url" : "medium-2.png",
+        },
+        {
+            "size" : "medium",
+            "url" : "medium-3.png",
+        },
+        {
+            "size" : "medium",
+            "url" : "medium-4.png",
+        },
+        {
+            "size" : "mediumplus",
+            "url" : "mediumplus-1.png",
+        },
+        {
+            "size" : "mediumplus",
+            "url" : "mediumplus-2.png",
+        },
+        {
+            "size" : "mediumplus",
+            "url" : "mediumplus-3.png",
+        },
+        {
+            "size" : "mediumplus",
+            "url" : "mediumplus-4.png",
+        },
+        {
+            "size" : "large",
+            "url" : "large-1.png",
+        },
+        {
+            "size" : "large",
+            "url" : "large-2.png",
+        },
+        {
+            "size" : "large",
+            "url" : "large-3.png",
+        },
+    ]
+    for (let j = 0; j < cards.length; j++) {
+        const cardWrap = document.querySelector(".flex-wrap");
+        newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "card-container");
+        newDiv.innerHTML=`
+            <div class="flip_box card" bloc-size="`+cards[j]['size']+`">
+                <div class="front front-img flex">
+                    <img class="flip" src="./src/blocks/`+cards[j]['url']+`">
+                </div>
+                <div class="back flex flex-column">
+                    <img src="./src/blocks/`+cards[j]['url']+`">
+                    <p class="flip point">test</p>
+                </div>    
+            </div>
+        `
+        cardWrap.appendChild(newDiv);
+    }
+}
+addCards();
 
 function randomizePoint(array){
     const max = parseInt(array[0]);
@@ -27,6 +107,9 @@ function reveal(){
                     break;
                 case 'medium':
                     point = randomizePoint(medium);
+                    break;
+                case 'mediumplus':
+                    point = randomizePoint(mediumplus);
                     break;
                 case 'large':
                     point = randomizePoint(large);
